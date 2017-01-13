01459d0af06c57f522c325a299f91f1c43928d79
Dzianis <dzianis.makeichyk@gmail.com>
2017-01-13 12:15:57 +0100

    youtube

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -144,7 +144,32 @@ $(document).ready(function() {
         $(".pop_up").delay(300).fadeIn(300);
     });
 
-  // Writing
+  // YouTube
+  var pop_up_video = $('#pop_up_video'),
+    pop_up_video_iframe = $('#pop_up_video_iframe'),
+    close_pop_up_video_id = $('#pop_up_video_bg'),
+    open_pop_up_video_id = $('#open_pop_up_video'),
+    mobile_open_pop_up_video_id = $('#mobile_open_pop_up_video');
 
-  // End Writind
+  // Pop-up Video
+
+  var close_pop_up_video = function(event){
+    event.preventDefault();
+    pop_up_video_iframe.attr('src', '');
+    pop_up_video.css('display', 'none');
+  };
+  close_pop_up_video_id.on('click', close_pop_up_video);
+
+
+  var open_pop_up_video = function(event){
+    event.preventDefault();
+    pop_up_video_iframe.attr('src', 'https://www.youtube.com/embed/daRLyB9rVC4');
+    pop_up_video.css('display', 'block');
+  };
+
+  open_pop_up_video_id.on('click', open_pop_up_video);
+  mobile_open_pop_up_video_id.on('click', open_pop_up_video);
+
+
+  // End YouTube
 });
